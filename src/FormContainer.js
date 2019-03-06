d68794355e5fd2f7866595e313754efd71534bab
Kyle Ma <32466847+kylemaa@users.noreply.github.com>
2019-03-05 19:54:33 -0800

    Added handlesubmit
    Still need to update Table Name. Edit Lambda func or make new Lamda function that ties to the same api

--- src/FormContainer.js
+++ src/FormContainer.js
@@ -12,6 +12,26 @@ class FormContainer extends Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleGridChange = this.handleGridChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const { rows, columns, clickedButtons } = this.state;
+
+    fetch("https://7afafg99v6.execute-api.us-west-2.amazonaws.com/api/tasks", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ rows, columns, clickedButtons })
+    });
+    this.setState({
+      rows: rows,
+      columns: columns,
+      clickedButtons: ["", ""]
+    });
+    e.target.reset();
   }
 
   handleChange(event) {
@@ -41,6 +61,7 @@ class FormContainer extends Component {
           handleChange={this.handleChange}
           handleGridChange={this.handleGridChange}
         />
+        <button onClick={this.props.handleChange}>Submit</button>
         {this.state.grid}
         {console.log(this.state)}
         {console.log(JSON.stringify(this.state))}
